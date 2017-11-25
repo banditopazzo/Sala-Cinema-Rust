50718e6e57e5684c0f00d96c2955e32d2a153bd3
banditopazzo <banditopazzo@gmail.com>
2017-11-25 17:32:28 +0100

    server uses futures pool

--- src/server/main.rs
+++ src/server/main.rs
@@ -3,6 +3,7 @@ extern crate mongodb;
 extern crate sala_cinema;
 extern crate chrono;
 extern crate futures;
+extern crate futures_cpupool;
 extern crate tokio_core;
 extern crate tokio_io;
 extern crate tokio_serde_json;
@@ -19,10 +20,14 @@ use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 
 use futures::{Sink, Stream};
-use futures::future::Future;
+use futures::future::{Executor,Future};
+use futures::sync::mpsc;
+use futures::sync::mpsc::{Sender, Receiver};
+use futures_cpupool::CpuPool;
+
 
 use tokio_core::net::TcpListener;
-use tokio_core::reactor::Core;
+use tokio_core::reactor::{Handle, Core};
 use tokio_core::net::TcpStream;
 
 use bytes::BytesMut;
@@ -34,30 +39,33 @@ use tokio_io::AsyncRead;
 use serde_json::Value;
 use tokio_serde_json::{ReadJson, WriteJson};
 
+use std::thread;
+
 fn main() {
+
+    const DB_NAME: &str = "RustProva";
+    const COLLECTION_NAME: &str = "prenotazione";
+    const NUM_THREADS: i32 = 4;
 
     //Connect to MongoDB
     let mongo = Client::connect("localhost", 27017)
         .expect("Failed to initialize standalone client.");
 
-    //Get the collection from the Database
-    let mongo_coll = mongo.db("RustProva").collection("prenotazione");
-
-    //Initialize the collection service
-    let mut coll = PrenotazioneCollection { mongo_coll };
-
     // Create the event loop that will drive this server
     let mut core = Core::new().unwrap();
     let handle = core.handle();
+
+    // set up a thread pool
+    let pool = CpuPool::new_num_cpus();
 
     // Bind the server's socket
     let addr = "127.0.0.1:12345".parse().unwrap();
     let listener = TcpListener::bind(&addr, &handle).unwrap();
 
     // Pull out a stream of sockets for incoming connections
-    let server = listener.incoming().for_each(|(sock, _)| {
+    let server = listener.incoming().for_each(|(sock, address)| {
 
-        println!("Connected 1 user");
+        println!("Connected 1 user. Addr: {:?}", address);
 
         let (reader, writer) = sock.split();
 
@@ -72,10 +80,33 @@ fn main() {
         let deserialized = ReadJson::<_, Value>::new(length_delimited)
             .map_err(|e| println!("ERR: {:?}", e));
 
-        // Spawn a concurrent task that prints all received messages to STDOUT
-        handle.spawn(deserialized.for_each(move |msg| {
+        //Clone db
+        let mongo = mongo.clone();
+
+        //Channel between the main thread and the future pool
+        let (sender, mut receiver) = mpsc::channel(0);
+
+        // Spawn a concurrent task that wait for future response and send result to the client
+        handle.spawn(receiver.for_each( move |response| {
 
             let serialized = &mut serialized;
+
+            //Send response to client
+            match serialized.send(response).wait() {
+                Ok(_) => println!("SENDED: to client"),
+                Err(_) => println!("Error sending message")
+            };
+
+            Ok(())
+
+        }));
+
+        // Execute the future in the pool
+        pool.execute(deserialized.for_each(move |msg| {
+
+            //Set-up DB service
+            let mongo_coll = mongo.clone().db(DB_NAME).collection(COLLECTION_NAME);
+            let db_service = PrenotazioneCollection { mongo_coll };
 
             let messaggio_ricevuto: Result<Message, _> = serde_json::from_value(msg);
             let messaggio_ricevuto = match messaggio_ricevuto {
@@ -86,17 +117,17 @@ fn main() {
             let response = match messaggio_ricevuto {
                 Message::GetMap => Message::Quit,
                 Message::Prenota(posto) => Message::Quit,
-                Message::Delete(id) => Message::Quit,
+                Message::Delete(id) => elimina_posto(&db_service, &id),
                 Message::Quit => Message::Quit,
                 Message::Error(err) => Message::Error(err),
                 _ => Message::Error(String::from("Messaggio non accettato"))
             };
 
-            // Create new message and send to the client, simulating a response
+            let sender = sender.clone();
             let jsoned = serde_json::to_value(&response).unwrap();
-            match serialized.send(jsoned).wait() {
-                Ok(_) => println!("SENDED: {:?}", response),
-                Err(_) => println!("Error sending message")
+            match sender.send(jsoned).wait() {
+                Ok(_) => println!("SENDED response to main thread"),
+                Err(e) => println!("Error sending message to the main thread: {:?}", e.description())
             };
 
             Ok(())
